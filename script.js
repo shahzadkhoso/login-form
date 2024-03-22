d74754e15965a217ce8ca36bf38208020c8a3658
shahzadkhoso <160629834+shahzadkhoso@users.noreply.github.com>
2024-03-22 10:56:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Here you can implement login functionality
+    alert('Login form submitted!');
+  });
+  
+  document.getElementById('registerForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Here you can implement registration functionality
+    alert('Registration form submitted!');
+  });
+  
